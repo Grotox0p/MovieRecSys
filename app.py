f96472ed1365a11b0662b9718f72d0194bb45e95
Grotox0p <93038945+Grotox0p@users.noreply.github.com>
2024-06-07 02:44:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def recommend(movie):
 
 st.header('Akhil Recommends You')
 movies = pickle.load(open('movie_list.pkl', 'rb'))
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+similarity = pickle.load(open('similar.pkl', 'rb'))
 
 
 movie_list = movies['title'].values
